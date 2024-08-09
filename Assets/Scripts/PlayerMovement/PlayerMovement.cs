35936cde7e3dd76acb89324e4a62af4d6456321a
noatuh <noatuh@outlook.com>
2024-08-09 13:03:19 -0400

    added sprinting

--- Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -8,6 +8,7 @@ namespace MyGameNamespace
     public class PlayerMovement : NetworkBehaviour
     {
         public float moveSpeed = 3.0f;
+        public float sprintSpeed = 6.0f;
 
         void Update()
         {
@@ -19,9 +20,12 @@ namespace MyGameNamespace
 
         void HandleMovement()
         {
+            // Determine the current speed
+            float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : moveSpeed;
+
             // Keyboard input for movement
-            float x = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-            float z = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+            float x = Input.GetAxis("Horizontal") * Time.deltaTime * currentSpeed;
+            float z = Input.GetAxis("Vertical") * Time.deltaTime * currentSpeed;
 
             // Apply transformations
             transform.Translate(x, 0, z);
